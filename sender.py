ef260bb689b5cb495522aee02475372ecbf57149
ochekhniy <chekhniy@ukr.net>
2018-09-25 16:09:22 +0300

    the base

---
+++ sender.py
@@ -0,0 +1,23 @@
+import socket
+import time
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+PORT = 5040
+
+sock.connect(('localhost', PORT))
+
+counter = 0
+
+print("Start sending")
+while True:
+    if counter > 20:
+        break
+    time.sleep(10)
+    counter += 1
+    print("The ", counter, " try")
+    message = 'hello, world! Try number: {}'.format(counter)
+    sock.send(message.encode())
+
+sock.close()
